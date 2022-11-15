d676f6cf14e99a14a6195301b039a951ed38ff33
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-11-15 08:00:41 -0500

    added exercise2

---
+++ exercise2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("A: ");
+int A = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (A < B) 
+{
+    if(A < B)
+    {
+        Console.WriteLine("min: " + A);
+    }
+    else
+    {
+        Console.WriteLine("min: " + B);
+    }
+}
+else if (A > B)
+{
+    {
+    Console.WriteLine("max: " + A);
+    }
+}
+    else
+    {
+        Console.WriteLine("max: " + B);
+    }
