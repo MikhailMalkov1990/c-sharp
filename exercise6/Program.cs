556e7d6d49f26e5440a88e033f86f133abc9bcc1
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-11-15 08:01:53 -0500

    added exercise6

---
+++ exercise6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("even number: " + num);
+}
+else
+{
+Console.WriteLine("uneven number: "+ num);
+}
