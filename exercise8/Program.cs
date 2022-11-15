38fcb6f034d3a087bdb1f5936b16486f907ea210
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-11-15 08:02:27 -0500

    added exercise8

---
+++ exercise8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int x = 1;
+while (x <= n) 
+{
+    if (x % 2 == 0)
+    Console.WriteLine($"even number: {x}");
+    x = x + 1;
+}
