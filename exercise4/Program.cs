112222248da6d7a10cc06001be82d590892d867b
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-11-15 08:01:20 -0500

    added exercise4

---
+++ exercise4/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Add first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("add second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("add third number: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    if(firstNumber > thirdNumber)
+    {
+        Console.WriteLine("max number: " + firstNumber);
+    }
+    else
+    {
+        Console.WriteLine("max number: " + thirdNumber);
+    }
+}
+else if (secondNumber > thirdNumber)
+{
+    Console.WriteLine("max number: " + secondNumber);
+}
+else 
+    {
+        Console.WriteLine("max number: " + thirdNumber);
+    }
